cf09717eb2e194cc92ef89629f7b9eaf1109f207
Kevin Mason <krmason2020@gmail.com>
2021-12-07 13:59:36 -0800

    updates to button component

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,15 +1,34 @@
 import React from "react";
 
+import Button from "./Button";
+
 import "components/Application.scss";
 
 export default function Application(props) {
+  const clickHandler = () => {
+    console.log("button-clicked");
+  };
   return (
     <main className="layout">
       <section className="sidebar">
-        {/* Replace this with the sidebar elements during the "Project Setup & Familiarity" activity. */}
+        <img
+          className="sidebar--centered"
+          src="images/logo.png"
+          alt="Interview Scheduler"
+        />
+        <hr className="sidebar__separator sidebar--centered" />
+        <nav className="sidebar__menu"></nav>
+        <img
+          className="sidebar__lhl sidebar--centered"
+          src="images/lhl.png"
+          alt="Lighthouse Labs"
+        />
       </section>
       <section className="schedule">
         {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        <Button onClick={clickHandler} disabled={false}>
+          Default
+        </Button>
       </section>
     </main>
   );
